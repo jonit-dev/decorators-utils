8477d2939d5b16d0eae9591b1adc9370143684f2
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-07-16 20:23:27 -0700

    initial commit

---
+++ src/DecorateAllWith.ts
@@ -0,0 +1,29 @@
+export function DecorateAllWith(decorator: MethodDecorator): ClassDecorator {
+  return function(constructor: Function) {
+    // Get the prototype of the class
+    const prototype = constructor.prototype;
+    // Get all property names of the class
+    const propertyNames = Object.getOwnPropertyNames(prototype);
+    // Loop over all property names
+    propertyNames.forEach(propertyName => {
+      // If the property is a function (but not the constructor)
+      if (
+        propertyName !== 'constructor' &&
+        typeof prototype[propertyName] === 'function'
+      ) {
+        // Get the descriptor of the method
+        const descriptor = Object.getOwnPropertyDescriptor(
+          prototype,
+          propertyName
+        );
+        // Decorate the method
+        const newDescriptor =
+          // @ts-ignore
+          decorator(prototype, propertyName, descriptor) || descriptor;
+        // Replace the method with the decorated method
+        // @ts-ignore
+        Object.defineProperty(prototype, propertyName, newDescriptor);
+      }
+    });
+  };
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { DecorateAllWith } from './DecorateAllWith';
+import { TrackClassExecutionTime } from './TrackClassExecutionTime';
+import { TrackExecutionTime } from './TrackExecutionTime';
+
+export { DecorateAllWith, TrackClassExecutionTime, TrackExecutionTime };
